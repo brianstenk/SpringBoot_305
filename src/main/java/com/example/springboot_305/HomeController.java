c17663e7f4489b6aecde1ca730cf9abb7c7a3bdd
Brian Nkurunungi <nkurunungib@gmail.com>
2020-10-05 09:34:08 -0500

    Many to Many Mapping

---
+++ src/main/java/com/example/springboot_305/HomeController.java
@@ -0,0 +1,39 @@
+package com.example.springboot_305;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+    @Autowired
+    ActorRepository actorRepository;
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        Actor actor = new Actor();
+        actor.setName("Sandra Bullock");
+        actor.setRealName("Sandra Mae Bullock");
+
+        Movie movie = new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+
+        movie.setDescription("Movie about Emojis...");
+        Set<Movie> movies = new HashSet<Movie>();
+        movies.add(movie);
+
+        actor.setMovies(movies);
+        actorRepository.save(actor);
+
+        model.addAttribute("actors", actorRepository.findAll());
+
+        return "index";
+    }
+}
